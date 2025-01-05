ac833e265ae50f8e2c72cff8a66d2d9515874b29
Joe Stopansky <joseph.stopansky@sunrun.com>
2025-01-05 12:13:02 -0500

    Add functions to generate computer choice and get human choice

---
+++ rps.js
@@ -0,0 +1,36 @@
+function getComputerChoice() {
+    const randomNumber = Math.floor(Math.random() * 3);
+    if (randomNumber === 0) {
+        computerChoice = "rock";
+    }
+    else if (randomNumber === 1) {
+        computerChoice = "paper";
+    }
+    else if (randomNumber === 2) {
+        computerChoice = "scissors";
+    }
+    else {
+        computerChoice = "invalid";
+    }
+    return computerChoice;
+}
+
+function getHumanChoice() {
+    let humanChoice;
+    let choiceIsValid = false;
+    while (!choiceIsValid) {
+        const userInput = prompt("Choose one: Rock, Paper, or Scissors");
+        const lowerInput = userInput.toLowerCase();
+        if (lowerInput === "rock" || lowerInput === "paper" || lowerInput === "scissors") {
+            humanChoice = lowerInput;
+            choiceIsValid = true;
+        }
+        else {
+            alert("Invalid selection, please try again.");
+        }
+    }
+    return humanChoice;
+}
+
+console.log(getComputerChoice());
+console.log(getHumanChoice());
